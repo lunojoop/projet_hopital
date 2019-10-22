0e5519ba0b7f0aa5c02a56773c29e849135656e0
luno joop <asanaliou@gmail.com>
2019-10-22 12:02:57 +0000

    j'ai refait la page d'acueil et le css de la page d'accueil

--- php/hopital.php
+++ php/hopital.php
@@ -8,27 +8,32 @@
 <body>
 	<nav class="navbar">
 		<ul class="aligne">
-			<li><a href="hopital.php">Nabil Choucair</a></li>
+			
 			<li class="decale"><a href="hopital.php">ACCUEIL</a></li>
-			<li class="decale"><a href="rendez-vous.php">Prendre un rendez-vous</a></li>
-			<li class="decale"><a href="inscrire.php">S'inscrire</a></li>
-			<li class="decale"><a href="connecter.php">Se connecter</a></li>
+			<li class="decale"><a href="">Administrateur</a></li>
+			<li class="decale"><a href="">Secrétaire</a></li>
+			<li class="decale"><a href="">MÉDECIN</a></li>
 		</ul>
 	</nav>
 	<h1>Nabil Choucair</h1>
 	<p>La plateforme Nabi houcair est une plateforme qui permet la gestion de la prise de rendez vous d'un hopital</p>
-	<section class="gauche">
-		<h2>ADMIN</h2>
-		<a href="connectadmin.php">SE CONNECTER</a>
-	</section>
-	<section class="milieu">
-		<h2>SECRETAIRE</h2>
-		<a href="connectsecretaire.php">SE CONNECTER</a>
-	</section>
-	<section class="droite">
-		<h2>MEDECIN</h2>
-		<a href="connectmedecin.php">SE CONNECTER</a>
-	</section>
-
+	<div id="continu">
+		<section class="gauche">
+			<img src="admin1.jpg">
+			<h2>ADMIN</h2>
+			<a href="connectadmin.php">SE CONNECTER</a>
+			
+		</section>
+		<section class="milieu">
+			<img src="secretaire-medicale-metier.jpg">
+			<h2>SECRETAIRE</h2>
+			<a href="connectsecretaire.php">SE CONNECTER</a>
+		</section>
+		<section class="droite">
+			<img src="docfond.jpg">
+			<h2>MEDECIN</h2>
+			<a href="connectmedecin.php">SE CONNECTER</a>
+		</section>
+	</div>
 </body>
 </html>
